3b550fadb453d38b5cff5a2c42586268ededd91e
haotengteng <1798440444@qq.com>
2023-11-10 16:19:27 +0800

    add:测试k8s镜像发布

--- mina-boot-example/mina-boot-example-docker/src/main/java/cn/mina/boot/example/common/docker/controller/PostgresController.java
+++ mina-boot-example/mina-boot-example-docker/src/main/java/cn/mina/boot/example/common/docker/controller/PostgresController.java
@@ -1,23 +1,23 @@
-package cn.mina.boot.example.common.docker.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @author Created by haoteng on 2022/7/17.
- */
-@RestController
-@RequestMapping("/postgres")
-public class PostgresController {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @RequestMapping(path = "user")
-    public String getUserInfo() {
-        Integer id = 1;
-        return jdbcTemplate.queryForObject("select name from user_info where id = ?", String.class, new Object[]{id});
-    }
-
-}
+//package cn.mina.boot.example.common.docker.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @author Created by haoteng on 2022/7/17.
+// */
+//@RestController
+//@RequestMapping("/postgres")
+//public class PostgresController {
+//    @Autowired
+//    private JdbcTemplate jdbcTemplate;
+//
+//    @RequestMapping(path = "user")
+//    public String getUserInfo() {
+//        Integer id = 1;
+//        return jdbcTemplate.queryForObject("select name from user_info where id = ?", String.class, new Object[]{id});
+//    }
+//
+//}
